f6490ccf6b7b3d2ef0013637fa728c2b636aa729
Jakesfot <singviy@qq.com>
2015-07-11 17:01:27 +0800

    优化代码
    修改了部分代码，将火箭图片本地化了。

--- BackToUp.js
+++ BackToUp.js
@@ -1,28 +1,33 @@
-
+//当页面往下拉距离顶部有或大于300px时，显示按钮，否则隐藏
 window.onscroll = function(){ 
-    var t = document.documentElement.scrollTop || document.body.scrollTop;  
-    var top_div = document.getElementById( "go" ); 
+    var t = document.documentElement.scrollTop || document.body.scrollTop;
+    var go = document.getElementById( "go" ); 
     if( t >= 300 ) { 
-        top_div.style.display = "block"; 
+		//alert(t);
+        go.style.display = "block"; 
     } else { 
-        top_div.style.display = "none"; 
+        go.style.display = "none"; 
     } 
-} //监听窗口滚动事件
+}
+
+//go按钮的点击函数，执行效果。
 var ptt=0;
-var btu = document.getElementById('flypig');
+var rocket = document.getElementById('flypig');
+
 function pageScroll() {
-btu.style.display='block';
+rocket.style.display='block';
 window.scrollBy(0,-10); //每次上升10px
 scrolldelay = setTimeout('pageScroll()',2); //2毫秒重复执行一次
 if(document.body.scrollTop==0) {
-  btu.style.marginBottom=ptt;
+//	alert('baktoup');
+  rocket.style.marginBottom=ptt+'px';
   ptt=ptt+10;
 }//火箭向上飞的效果
 
 if(ptt>=1000){
   ptt=0;
-  btu.style.marginBottom=20;
-  btu.style.display='none';
+  rocket.style.marginBottom=20;
+  rocket.style.display='none';
   clearTimeout(scrolldelay);
 }//返回原始状态
 
